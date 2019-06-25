88bece6692cc8a1d9709999e64a7809926e8e114
nk5 <nkws27@gmail.com>
2019-06-25 22:01:32 +0900

    init project

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by 中川慶悟 on 2019/06/25.
+//  Copyright © 2019 Keigo Nakagawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
